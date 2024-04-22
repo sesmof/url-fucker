50d942611428d198f32bfedf44e0b96db95fd859
sesmof <165493993+sesmof@users.noreply.github.com>
2024-04-22 11:42:32 +0800

    找子域名的模块(来源于jsfinder)

---
+++ core/findsudomain.py
@@ -0,0 +1,27 @@
+#this file comes from JSfinder:https://github.com/Threezh1/JSFinder/blob/master/JSFinder.py
+from urllib.parse import urlparse
+def find_last(string,str):
+	positions = []
+	last_position=-1
+	while True:
+		position = string.find(str,last_position+1)
+		if position == -1:break
+		last_position = position
+		positions.append(position)
+	return positions
+def find_subdomain(urls, mainurl):
+	url_raw = urlparse(mainurl)
+	domain = url_raw.netloc
+	miandomain = domain
+	positions = find_last(domain, ".")
+	if len(positions) > 1:miandomain = domain[positions[-2] + 1:]
+	subdomains = []
+	for url in urls:
+		suburl = urlparse(url)
+		subdomain = suburl.netloc
+		#print(subdomain)
+		if subdomain.strip() == "": continue
+		if miandomain in subdomain:
+			if subdomain not in subdomains:
+				subdomains.append(subdomain)
+	return subdomains
